661f1d9f5c744df29d4db60a47c0f1084683dc44
Júnior de Oliveira <ellismarjr@hotmail.com>
2020-08-09 07:10:22 -0300

    landing page finished

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Landing from './pages/Lading';
+
+import './assets/styles/global.css'
+
 function App() {
   return (
-    <div className="App">
-     <h1>App</h1>
-    </div>
+    <Landing />
   );
 }
 
