333fdbc8a78bd9d4227c185159b0f1e240df08bf
ellismarjr <ellismarjr@hotmail.com>
2020-09-02 12:26:08 -0300

    refact

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  response.json({message: 'Acessou a rota'})
+})
+
 app.listen(3333);
